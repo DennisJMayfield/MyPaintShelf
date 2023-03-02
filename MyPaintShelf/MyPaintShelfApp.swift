959c8657d83990585269c73e7fc06775e8cf627e
Djmayfield1 <dennisjmayfield@gmail.com>
2023-03-01 18:26:09 -0800

    Initial Commit

---
+++ MyPaintShelf/MyPaintShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPaintShelfApp.swift
+//  MyPaintShelf
+//
+//  Created by Dennis Mayfield on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPaintShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
