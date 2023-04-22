7260428cdb32e95b596704e2ab892780c6aff11c
Djmayfield1 <dennisjmayfield@gmail.com>
2023-04-22 16:11:16 -0700

    Change navigation bar title, and add button.

--- MyPaintShelf/ContentView.swift
+++ MyPaintShelf/ContentView.swift
@@ -7,8 +7,18 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    init() {
+           //Use this if NavigationBarTitle is with Large Font
+           //UINavigationBar.appearance().largeTitleTextAttributes = [.font : UIFont(name: "Georgia-Bold", size: 20)!]
+
+           //Use this if NavigationBarTitle is with displayMode = .inline
+        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "AcademyEngravedLetPlain", size: 34)!]
+       }
     
+    @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var paints: FetchedResults<Paint>
     @State private var showingaddPaint = false
     @State private var name: String = ""
@@ -20,11 +30,16 @@ struct ContentView: View {
                     Text("Paint")
                     
                 }
-                .navigationTitle("MyPaintShelf")
-                Button("Add Paint") {
-                    showingaddPaint = true
-                }
-                .buttonStyle(.borderedProminent)
+                //.navigationBarTitle(Text("Dashboard").font(.subheadline), displayMode: .large)
+                .navigationBarTitle (Text("My Paint Shelf").font(.largeTitle), displayMode: .inline)
+                
+                .listStyle(.insetGrouped)
+                Button(action: {}, label: {
+                    HStack {
+                        Image(systemName: "plus.circle")
+                        Text("Add new paint")
+                    }
+                })
                 .popover(isPresented: $showingaddPaint) {
                     TextField("Enter your paint name.", text:$name)
                         .font(.headline)
@@ -35,6 +50,7 @@ struct ContentView: View {
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
